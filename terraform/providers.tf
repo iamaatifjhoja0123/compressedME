e7af26914e5d28a8c55a3c1beee43ce2f22e28a6
Aatif Jhoja <m.aatif0123@gmail.com>
2026-04-19 16:44:50 +0530

    Fix: Added AWS S3 remote backend for Terraform state

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,4 +1,11 @@
 terraform {
+  # Yeh backend block Terraform ko batata hai ki state file AWS S3 mein save karni hai
+  backend "s3" {
+    bucket = "compressedme-tfstate-aatif"
+    key    = "prod/terraform.tfstate"
+    region = "ap-south-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
